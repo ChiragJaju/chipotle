837325c49deb8efa577e38ba96b5b7268f52c7a4
plaidbait91 <f20200055@hyderabad.bits-pilani.ac.in>
2022-04-15 17:25:34 +0530

    Create endpoints for login, menu and orders

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql')
 // const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -12,7 +13,7 @@ const corsOpts = {
   allowedHeaders: ["Content-Type"],
   exposedHeaders: ["Content-Type"],
 };
-// app.use(cors(corsOpts));
+app.use(cors(corsOpts));
 // mongoose.connect(process.env.MDB_CONNECT, {
 //   useNewUrlParser: true,
 
@@ -32,3 +33,72 @@ app.use(express.json({ limit: "50mb" }));
 app.listen(5000, () => {
   console.log("Server is running at port 5000");
 });
+
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'test'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
+
+app.post('/login', (req, res) => {
+  const user = req.query.user
+  const email = req.query.email
+
+  connection.query("select * from user where uname = ? and email = ?", [user, email], function (error, results, fields) {
+    if (error) throw error;
+    if(results.length > 0) res.send(results)
+    else {
+
+      connection.query("insert into user values(NULL, ?, ?, 'Valmiki', 320)", [email, user], function (error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+      });
+      
+    }
+  });
+})
+
+app.get('/menu', (req, res) => {
+  const veg = req.query.veg
+
+  let query = "select * from menuitem";
+  if(veg) query += " where isveg = true";
+  connection.query(query, function (error, results, fields) {
+    if (error) throw error;
+    if(results.length > 0) res.send(results)
+    else {
+      res.send("No menu yet")
+    }
+  });
+})
+
+app.post('/order', (req, res) => {
+  
+  const order = req.body;
+
+  connection.query("insert into orders values (NULL, ?, ?, 'Preparing', ?, 1000, 11)", [order.orderTime, order.orderTime + 1800, order.deliveryOption], function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+})
+
+app.get('/order-history', (req, res) => {
+  
+  const uid = req.query.id;
+
+  connection.query("select * from orders where ordered_by = ? and status = 'Done'", [uid], function (error, results, fields) {
+    if(results.length > 0) res.send(results)
+    else {
+      res.send("No completed orders")
+    }
+  });
+})
